72e4dafa5ada97312d2ce3cf8a2674af722e2e20
Logan Smith <logsmith06@gmail.com>
2020-12-17 19:14:26 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const listTask = document.querySelector("#tasks");
+
+  // prevents form submission
+  const submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+    listTask.innerHTML +=
+     `<li> ${document.getElementById('new-task-description').value}
+      <button data-action = "delete"> X </button>
+      </li>
+     `;
+
+  });
+
+  listTask.addEventListener("click", function(bye){
+    if (bye.target.dataset.action === "delete"){
+      bye.target.parentElement.remove();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
